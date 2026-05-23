4cbd10767435f1b121a662085ca6d29b9e44066b
Pedro Santos <pedrosantos@MacBook-Pro-de-Pedro.local>
2026-05-23 18:25:46 -0300

    Logo height 120

--- app/components/WePromptLogo.js
+++ app/components/WePromptLogo.js
@@ -3,7 +3,7 @@ export default function WePromptLogo({ id, textColor }) {
     <img
       src="/logo.png"
       alt="WePrompt"
-      style={{ height: 80, width: "auto", display: "block", maxWidth: "none" }}
+      style={{ height: 120, width: "auto", display: "block", maxWidth: "none" }}
     />
   );
 }
