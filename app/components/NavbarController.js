ed0728eb9ec7f1403794ff9aa76886c80b85a5d2
Pedro Santos <pedrosantos@MacBook-Pro-de-Pedro.local>
2026-05-28 19:28:51 -0300

    Hide navbar on /admin routes

--- app/components/NavbarController.js
+++ app/components/NavbarController.js
@@ -2,7 +2,7 @@
 import { usePathname } from "next/navigation";
 import Navbar from "./Navbar";
 
-const HIDDEN_PREFIXES = ["/dashboard", "/login", "/cadastro", "/completar-perfil", "/checkout", "/obrigado"];
+const HIDDEN_PREFIXES = ["/dashboard", "/login", "/cadastro", "/completar-perfil", "/checkout", "/obrigado", "/admin"];
 const HIDDEN_EXACT = ["/"];
 
 export default function NavbarController() {
